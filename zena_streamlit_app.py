0a8a2c26952273c0c08354ff5a1be882f28eead1
Priyanka <110306658+Priyanka3101@users.noreply.github.com>
2022-09-28 15:26:49 +0530

    Update zena_streamlit_app.py

--- zena_streamlit_app.py
+++ zena_streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import snowflake.connector
 
 streamlit.title('Zenas Web Catalog')
-"""
+#"""
 my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur=my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION() ")
@@ -17,3 +17,4 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+"""
